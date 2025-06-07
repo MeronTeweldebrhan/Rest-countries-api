78307d36cf3ec65ae784a9b379bbb4bc063df73c
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-06 22:22:44 -0400

    Add Display fucntionality

--- src/js/country-detail.js
+++ src/js/country-detail.js
@@ -31,3 +31,31 @@ async function fetchCountryDetail() {
     console.log(error);
   }
 }
+function displayCountryDetail(country,allCountries) {
+  try {
+    const card = document.createElement("div");
+    card.className = "country-details";
+    card.innerHTML = `
+      <img src="${country.flags.svg}" alt="${country.name.common} flag" class="country-flag" />
+      <div class="country-info">
+        <h2>${country.name.common}</h2>
+        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+        <p><strong>Region:</strong> ${country.region}</p>
+        <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
+        <p><strong>Subregion:</strong> ${country.subregion}</p>
+        <p><strong>Native Name:</strong> ${country.name.nativeName ? Object.values(country.name.nativeName)[0].common : "N/A"}</p>
+        <p><strong>Currency:</strong> ${country.currencies ? Object.values(country.currencies)[0].name : "N/A"}</p>
+        <p><strong>Top Level Domain:</strong> ${country.tld ? country.tld[0] : "N/A"}</p>
+        <p><strong>Common Languages:</strong> ${country.languages ? Object.values(country.languages).join(", ") : "N/A"}</p>
+         <p><strong>currencies: ${country.currencies ? Object.values(country.currencies)[0].name : "N/A"}</p>
+        <p><strong>Border Countries:</strong> <span id="border-countries"></span></p>
+      </div>
+    `;
+    countryDetail.innerHTML = "";
+    countryDetail.appendChild(card);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchCountryDetail(countryName);
